c4ac26105fab641e62f1d162934416c026c35713
Alex <alex.reardon14@gmail.com>
2018-11-08 23:50:43 -0500

    Updates

---
+++ precache-manifest.b78de382447e0995559f2cb96be71d3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f98261544c940b0eaf52fa510fce0b16",
+    "url": "/react-celtics-players/static/media/logo.f9826154.png"
+  },
+  {
+    "revision": "eb736434ae7c0a98362e",
+    "url": "/react-celtics-players/static/js/runtime~main.eb736434.js"
+  },
+  {
+    "revision": "9915a0c01106387b24e8",
+    "url": "/react-celtics-players/static/js/main.9915a0c0.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/react-celtics-players/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "9915a0c01106387b24e8",
+    "url": "/react-celtics-players/static/css/main.097930c4.chunk.css"
+  },
+  {
+    "revision": "4bf35559849727e2116fb5782f1456d3",
+    "url": "/react-celtics-players/index.html"
+  }
+];
