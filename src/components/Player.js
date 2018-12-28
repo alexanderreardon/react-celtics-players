e5d91a921e7cafa7fe8b8e7e66df4a03e5d38e6c
Alex <alex.reardon14@gmail.com>
2018-12-27 22:50:07 -0500

    Condensed Player component; removed large if statement in render.

--- src/components/Player.js
+++ src/components/Player.js
@@ -11,166 +11,85 @@ class Player extends React.Component {
         }
     }
     
-    render(){
-        var player_image = this.props.image;
-        if (player_image) {
-            return (
-                <div className="player">
-                    <div className="header">
-                        <div className="header-left">
-                            <h3 className="player-name" >{this.props.name}</h3>
-                            <p className="birthday">{this.props.birthDate}</p>
-                            <p className="seasons-played">{this.getSeasons()}</p>
-                            <p className="position">{this.props.position}</p>
-                            <div className="weight-height">
-                                <p className="weight">{this.props.weight} lbs.</p>
-                                <p className="height">{this.props.height}</p>
-                            </div>
-                        </div>
-                        <div className="header-right">
-                            <img src={this.props.image} width="100" alt={this.props.name} />
+    render() {
+        return (
+            <div className="player">
+                <div className="header">
+                    <div className="header-left">
+                        <h3 className="player-name" >{this.props.name}</h3>
+                        <p className="birthday">{this.props.birthDate}</p>
+                        <p className="seasons-played">{this.getSeasons()}</p>
+                        <p className="position">{this.props.position}</p>
+                        <div className="weight-height">
+                            <p className="weight">{this.props.weight} lbs.</p>
+                            <p className="height">{this.props.height}</p>
                         </div>
                     </div>
-                    <div className="player-stats">
-                        <div className="stats-left">
-                            <div className="games">
-                                <p className="label">Games Played:</p>
-                                <p className="stat">{this.props.gamesPlayed}</p>
-                            </div>
-                            <div className="minutes">
-                                <div className="total">
-                                    <p className="label">Minutes Played:</p>
-                                    <p className="stat">{this.props.minutesPlayed}</p>
-                                </div>
-                                <div className="pg">
-                                    <p className="label pergame">MPG:</p>
-                                    <p className="stat pergame">{this.props.minutesPerGame}</p>  
-                                </div>
-                            </div>
-                            <div className="points">
-                                <div className="total">
-                                    <p className="label">Total Points:</p>
-                                    <p className="stat">{this.props.points}</p>
-                                </div>
-                                <div className="pg">
-                                    <p className="label pergame">PPG:</p>
-                                    <p className="stat pergame">{this.props.pointsPerGame}</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="stats-right">
-                            <div className="steals">
-                                <p className="label">Steals:</p>
-                                <p className="stat">{this.props.steals}</p>
-                            </div>
-                            <div className="rebounds">
-                                <div className="total">
-                                    <p className="label">Rebounds:</p>
-                                    <p className="stat">{this.props.rebounds}</p>
-                                </div>
-                                <div className="pg">
-                                    <p className="label pergame">RPG:</p>
-                                    <p className="stat pergame">{this.props.reboundsPerGame}</p>
-                                </div>
-                            </div>
-                            <div className="assists">
-                                <div className="total">
-                                    <p className="label">Assists:</p>
-                                    <p className="stat">{this.props.assists}</p>
-                                </div>
-                                <div className="pg">
-                                    <p className="label pergame">APG:</p>
-                                    <p className="stat pergame">{this.props.assistsPerGame}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="footer">
-                        <a href={"https://basketball-reference.com" + this.props.url} className="stat-link">VIEW STATS</a>
+                    <div className="header-right">
+                        <img src={this.props.image ? this.props.image : require('../img/unknown.png')} width="100" alt={this.props.name} />
                     </div>
                 </div>
-            )
-        } else {
-            return (
-                <div className="player">
-                    <div className="header">
-                        <div className="header-left">
-                            <h3 className="player-name" >{this.props.name}</h3>
-                            <p className="birthday">{this.props.birthDate}</p>
-                            <p className="seasons-played">{this.getSeasons()}</p>
-                            <p className="position">{this.props.position}</p>
-                            <div className="weight-height">
-                                <p className="weight">{this.props.weight} lbs.</p>
-                                <p className="height">{this.props.height}</p>
+                <div className="player-stats">
+                    <div className="stats-left">
+                        <div className="games">
+                            <p className="label">Games Played:</p>
+                            <p className="stat">{this.props.gamesPlayed}</p>
+                        </div>
+                        <div className="minutes">
+                            <div className="total">
+                                <p className="label">Minutes Played:</p>
+                                <p className="stat">{this.props.minutesPlayed}</p>
+                            </div>
+                            <div className="pg">
+                                <p className="label pergame">MPG:</p>
+                                <p className="stat pergame">{this.props.minutesPerGame}</p>  
                             </div>
                         </div>
-                        <div className="header-right">
-                            <img src={require('../img/unknown.png')} width="100" alt={this.props.name} />
-                        </div>
-                    </div>
-                    <div className="player-stats">
-                        <div className="stats-left">
-                            <div className="games">
-                                <p className="label">Games Played:</p>
-                                <p className="stat">{this.props.gamesPlayed}</p>
+                        <div className="points">
+                            <div className="total">
+                                <p className="label">Total Points:</p>
+                                <p className="stat">{this.props.points}</p>
                             </div>
-                            <div className="minutes">
-                                <div className="total">
-                                    <p className="label">Minutes Played:</p>
-                                    <p className="stat">{this.props.minutesPlayed}</p>
-                                </div>
-                                <div className="pg">
-                                    <p className="label pergame">MPG:</p>
-                                    <p className="stat pergame">{this.props.minutesPerGame}</p>  
-                                </div>
-                            </div>
-                            <div className="points">
-                                <div className="total">
-                                    <p className="label">Total Points:</p>
-                                    <p className="stat">{this.props.points}</p>
-                                </div>
-                                <div className="pg">
-                                    <p className="label pergame">PPG:</p>
-                                    <p className="stat pergame">{this.props.pointsPerGame}</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="stats-right">
-                            <div className="steals">
-                                <p className="label">Steals:</p>
-                                <p className="stat">{this.props.steals}</p>
-                            </div>
-                            <div className="rebounds">
-                                <div className="total">
-                                    <p className="label">Rebounds:</p>
-                                    <p className="stat">{this.props.rebounds}</p>
-                                </div>
-                                <div className="pg">
-                                    <p className="label pergame">RPG:</p>
-                                    <p className="stat pergame">{this.props.reboundsPerGame}</p>
-                                </div>
-                            </div>
-                            <div className="assists">
-                                <div className="total">
-                                    <p className="label">Assists:</p>
-                                    <p className="stat">{this.props.assists}</p>
-                                </div>
-                                <div className="pg">
-                                    <p className="label pergame">APG:</p>
-                                    <p className="stat pergame">{this.props.assistsPerGame}</p>
-                                </div>
+                            <div className="pg">
+                                <p className="label pergame">PPG:</p>
+                                <p className="stat pergame">{this.props.pointsPerGame}</p>
                             </div>
                         </div>
                     </div>
-                    <div className="footer">
-                        <a href={"https://basketball-reference.com" + this.props.url} className="stat-link">VIEW STATS</a>
+                    <div className="stats-right">
+                        <div className="steals">
+                            <p className="label">Steals:</p>
+                            <p className="stat">{this.props.steals}</p>
+                        </div>
+                        <div className="rebounds">
+                            <div className="total">
+                                <p className="label">Rebounds:</p>
+                                <p className="stat">{this.props.rebounds}</p>
+                            </div>
+                            <div className="pg">
+                                <p className="label pergame">RPG:</p>
+                                <p className="stat pergame">{this.props.reboundsPerGame}</p>
+                            </div>
+                        </div>
+                        <div className="assists">
+                            <div className="total">
+                                <p className="label">Assists:</p>
+                                <p className="stat">{this.props.assists}</p>
+                            </div>
+                            <div className="pg">
+                                <p className="label pergame">APG:</p>
+                                <p className="stat pergame">{this.props.assistsPerGame}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                
-            )
-        }
+                <div className="footer">
+                    <a href={"https://basketball-reference.com" + this.props.url} className="stat-link">VIEW STATS</a>
+                </div>
+            </div>
+        )
+        
     }
-
 };
 
 export default Player;
